22e1cfca55a33f3c0a7f4bc691be72a44cea24d6
Farhan Akthar <tech_fur@outlook.com>
2018-11-29 11:20:42 +0530

    syntax c

--- src/main/java/springexample/Application.java
+++ src/main/java/springexample/Application.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static String DEFAULT_CONFIG_FILENAME = "config.properties";
     public static BraintreeGateway gateway;
+    // public static BraintreeGateway gateway;
 
     public static void main(String[] args) {
         File configFile = new File(DEFAULT_CONFIG_FILENAME);
